066760db0b132141c218f762d86a5a469155c3f1
Joshua Chima <45519648+JoshChima@users.noreply.github.com>
2020-02-05 12:14:58 -0600

    product service update

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,43 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-    @Post()
-    addProduct(): any {
-        
+    constructor(private readonly productsService: ProductsService) {
+
     }
+    //you could also specifiy a specific parameter in @Body() exp: @Body('title') productTiele: string
+    @Post()
+    addProduct(
+        @Body() completeBody: { title: string, desc: string, price: number }): any {
+        const generatedID = this.productsService.insertProduct(completeBody.title, completeBody.desc, completeBody.price);
+        return { id: generatedID };
+    }
+
+    @Get()
+    getAllProducts() {
+        return this.productsService.getAll();
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') prodID: string) {
+        return this.productsService.getSinleProduct(prodID);
+    }
+
+    @Patch(':id')
+    updateProduct(
+        @Param('id') prodId: string,
+        @Body('title') prodTitle: string,
+        @Body('desc') prodDesc: string,
+        @Body('price') prodPrice: number) {
+        this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+        return null
+    }
+
+    @Delete(':id')
+    removeProduct(@Param('id') prodId: string){
+        this.productsService.deleteProduct(prodId);
+        return null;
+    }
+
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
+import uuid = require('uuid-random');
 
 @Injectable()
 export class ProductsService {
-    products: Product[]
+    private products: Product[] = [];
+
+    insertProduct(title: string, desc: string, price: number): string {
+        const prodID = uuid()
+        const newProduct = new Product(prodID, title, desc, price);
+        this.products.push(newProduct);
+        return prodID;
+    }
+
+    getAll() {
+        //Json method makes a deep copy, no gateways into the products array
+        return JSON.parse(JSON.stringify(this.products))
+    }
+
+    getSinleProduct(productId: string) {
+        const product = this.findProduct(productId)[0];
+        return { ...product }
+    }
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+        const [product, index] = this.findProduct(productId);
+        const updatedProduct = { ...product, }
+        if (title) {
+            updatedProduct.title = title
+        }
+        if (desc) {
+            updatedProduct.desc = desc
+        }
+        if (price) {
+            updatedProduct.price = price
+        }
+        this.products[index] = updatedProduct;
+    }
+
+    deleteProduct(productId: string) {
+        const index = this.findProduct(productId)[1];
+        this.products.splice(index, 1);
+    }
+
+    private findProduct(id: string): [Product, number] {
+        const productIndex = this.products.findIndex((prod) => prod.id == id);
+        const product = this.products[productIndex];
+        if (!product) {
+            throw new NotFoundException('Could not find product')
+        }
+        return JSON.parse(JSON.stringify([product, productIndex]))
+    }
 }
